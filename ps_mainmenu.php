c1709c9ed668aa075c45b7741bb56666a4957116
okom3pom <contact@okom3pom.com>
2021-06-28 12:34:13 +0200

    Fix Helper error

--- ps_mainmenu.php
+++ ps_mainmenu.php
@@ -1077,7 +1077,7 @@ class Ps_MainMenu extends Module implements WidgetInterface
         $lang = new Language((int) Configuration::get('PS_LANG_DEFAULT'));
         $helper->default_form_language = $lang->id;
         $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') ? Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') : 0;
-        $helper->fields_form = [];
+        $this->fields_form = [];
         $helper->module = $this;
         $helper->identifier = $this->identifier;
         $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false) .
